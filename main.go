ad5ec80dfa368a5d85c672f23452b93656fc97e5
Yuqing2003 <114666281+Yuqing2003@users.noreply.github.com>
2022-10-22 19:13:08 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -12,3 +12,31 @@ func main() {
 		fmt.Println("空值")
 	}
 }
+
+//package main
+//
+//import "fmt"
+//
+//type Sayer interface {
+//	Say()
+//}
+//type dog struct {
+//}
+//type cat struct {
+//}
+//
+//func (d dog) Say() {
+//	fmt.Println("汪汪汪")
+//}
+//func (c cat) Say() {
+//	fmt.Println("喵喵喵")
+//}
+//func main() {
+//	var x Sayer
+//	a := cat{}
+//	b := dog{}
+//	x = a
+//	x.Say()
+//	x = b
+//	x.Say()
+//}
