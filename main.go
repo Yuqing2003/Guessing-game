2e1a86a32f5483b8d23092a4e3ab88019a812a51
Yuqing2003 <114666281+Yuqing2003@users.noreply.github.com>
2022-10-29 13:26:15 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -1,42 +1,86 @@
+//package main
+
+//func main() {
+//	好康的 := 欢迎来我家玩()
+//	打电动()
+//	fmt.Println(好康的)
+//}
+//func 欢迎来我家玩() string {
+//	time.Sleep(5 * time.Second)
+//	return "登dua郎
+//"
+//}
+
+//	func 打电动() {
+//		fmt.Println("输了啦，都是你害的")
+//
+// package main
+//
+// import (
+//
+//	"fmt"
+//	"time"
+//
+// )
+//
+// func main() {
+// var (
+// ch1  = make(chan struct{})
+// stop = make(chan struct{})
+// )
+// go handleCh1(ch1)
+// go func() {
+// time.Sleep(10 * time.Second)
+// stop <- struct{}{}
+// }()
+// LOOP:
+// for {
+// select {
+// case _ = <-ch1:
+// fmt.Println("get from ch1")
+// case _ = <-stop:
+// break LOOP
+// }
+// }
+// }
+//
+// func handleCh1(ch1 chan struct{}) {
+// for {
+// time.Sleep(3 * time.Second)
+// ch1 <- struct{}{}
+// }
+// }
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	var p *string
-	fmt.Println(p)
-	fmt.Println("p的值是%v\n", p)
-	if p != nil {
-		fmt.Println("非空")
-	} else {
-		fmt.Println("空值")
+	var (
+		ch1  = make(chan struct{})
+		stop = make(chan struct{})
+	)
+	go handleCh1(ch1)
+	go func() {
+		time.Sleep(10 * time.Second)
+		stop <- struct{}{}
+	}()
+LOOP:
+	for {
+		select {
+		case _ = <-ch1:
+			fmt.Println("get from ch1")
+		case _ = <-stop:
+			break LOOP
+		}
 	}
 }
 
-//package main
-//
-//import "fmt"
-//
-//type Sayer interface {
-//	Say()
-//}
-//type dog struct {
-//}
-//type cat struct {
-//}
-//
-//func (d dog) Say() {
-//	fmt.Println("汪汪汪")
-//}
-//func (c cat) Say() {
-//	fmt.Println("喵喵喵")
-//}
-//func main() {
-//	var x Sayer
-//	a := cat{}
-//	b := dog{}
-//	x = a
-//	x.Say()
-//	x = b
-//	x.Say()
-//}
+func handleCh1(ch1 chan struct{}) {
+	for {
+		time.Sleep(3 * time.Second)
+		ch1 <- struct{}{}
+	}
+}
